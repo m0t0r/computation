026c862f62fc496df22d6aa78c746250e5644a2f
m0t0r <tkachenko.vitaly.job@gmail.com>
2018-08-23 20:52:04 -0500

    feat: add 'while' looping statement

--- simple/main.go
+++ simple/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	var m1, m2, m3 machine.Machine
+	var m1, m2, m3, m4 machine.Machine
 
 	fmt.Print("Machine 1 Test - Statements Evaluation\n\n")
 
@@ -55,4 +55,19 @@ func main() {
 	}
 
 	m3.Run()
+
+	fmt.Print("Machine 4 Test - While Statement Evaluation\n\n")
+
+	env = make(map[string]expr.Expression)
+	env["x"] = expr.Number{Value: 1}
+
+	m4 = machine.Machine{
+		Statement: stm.While{
+			Condition: expr.LessThan{Left: expr.Variable{Name: "x"}, Right: expr.Number{Value: 5}},
+			Body:      stm.Assign{Name: "x", Expression: expr.Multiply{Left: expr.Variable{Name: "x"}, Right: expr.Number{Value: 3}}},
+		},
+		Environment: env,
+	}
+
+	m4.Run()
 }
